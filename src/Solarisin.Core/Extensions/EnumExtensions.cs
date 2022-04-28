2cf00a7294ff7779eb68b58569305fd770ad9b46
solarisin <solarisin@gmail.com>
2022-04-27 21:04:29 -0400

    feat: added Enum extension class and GetDescription function

---
+++ src/Solarisin.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Solarisin.Core.Extensions;
+
+/// <summary>
+/// Class extensions for <see cref="Enum"/>.
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    /// Retrieve the description attribute for the given enum value.
+    /// </summary>
+    /// <param name="value">The enum value to retrieve the description for.</param>
+    /// <returns>The description attribute string.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the enum value or attribute is missing.</exception>
+    public static string GetDescription(this Enum value)
+    {
+        var type = value.GetType();
+        var name = Enum.GetName(type, value) ??
+                   throw new InvalidOperationException($"Enum {type.Name} does not contain value {value}");
+        var field = type.GetField(name) ??
+                   throw new InvalidOperationException($"Enum {type.Name} does not contain field {name}");
+        var attribute = field.GetCustomAttribute<DescriptionAttribute>() ??
+                   throw new InvalidOperationException($"Enum {type.Name} does not contain description attribute for {name}");
+        return attribute.Description;
+    }
+}
